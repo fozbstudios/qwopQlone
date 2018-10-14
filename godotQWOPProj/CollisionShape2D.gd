518476e9d8b7532e53063318e394443dbdacb48f
Nick Kirkpatrick <nicholasmichaelkirkpatrick@gmail.com>
2018-10-14 16:32:03 -0500

    collisions not working

--- godotQWOPProj/CollisionShape2D.gd
+++ godotQWOPProj/CollisionShape2D.gd
@@ -7,23 +7,23 @@ extends CollisionShape2D
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	print("l")
-	print(self.transform.origin)
-	print(self.transform.x)
-	print(self.transform.y)
-	print("ln")
-	print(self.transform.origin.normalized())
-	print(self.transform.x.normalized())
-	print(self.transform.y.normalized())
-	print("g")
-	print(self.to_global(transform.origin))
-	print(self.to_global(transform.x))
-	print(self.to_global(transform.y))
-	print("gn")
-	print(self.to_global(transform.origin).normalized())
-	print(self.to_global(transform.x).normalized())
-	print(self.to_global(transform.y).normalized())
-	pass
+	# print("l")
+	# print(self.transform.origin)
+	# print(self.transform.x)
+	# print(self.transform.y)
+	# print("ln")
+	# print(self.transform.origin.normalized())
+	# print(self.transform.x.normalized())
+	# print(self.transform.y.normalized())
+	# print("g")
+	# print(self.to_global(transform.origin))
+	# print(self.to_global(transform.x))
+	# print(self.to_global(transform.y))
+	# print("gn")
+	# print(self.to_global(transform.origin).normalized())
+	# print(self.to_global(transform.x).normalized())
+	# print(self.to_global(transform.y).normalized())
+ pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
